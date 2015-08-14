9eaf594393a3e215fda107c06aedb9050bde2767
Arthur Xavier <arthur.xavierx@gmail.com>
2015-08-14 14:40:31 -0300

    Minor updates: removed languages, linkedin icon and changed colors

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $(window).on('scroll', function() {
     $('.progress:above-the-top, .progress:below-the-fold').css('width', '0');
     $('.progress-bar:in-viewport').each(function() {
-      var percent = parseFloat($(this).attr('percent'));
+      var percent = parseFloat($(this).data('percent'));
       $(this).find('.progress').animate({
         width: Math.round(percent*240) + 'px'
       }, 1000);
